7ee34d1ee4e092ea57b305e1ce4acb233692dd12
anggr <anggr@LAPTOP-BN71574J>
2021-12-13 00:01:39 +0700

    important

---
+++ WindowsApp1/Class1.vb
@@ -0,0 +1,5 @@
+﻿Public Class Class1
+    Public Shared SeatID As String
+    Public Shared SeatRow As String
+    Public Shared IsBooked As String
+End Class
---
+++ WindowsApp1/Module1.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Data.SqlClient
+Module ModuleConnect
+    Public connection As SqlConnection
+    Sub db_connect()
+        Try
+            connection = New SqlConnection("Server=localhost;Database=CIS_db;Integrated Security=True;MultipleActiveResultSets=true")
+            connection.Open()
+        Catch ex As Exception
+            MsgBox("Can't connect to database, " _
+                   + "Please Check your Connection")
+        End Try
+    End Sub
+End Module
